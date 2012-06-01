5a7149f933e8093808e60dad3aedfd8c0245609a
Colin Thomas-Arnold <colin@fusionbox.com>
2012-06-01 08:52:41 -0600

    fix fixnum uicolor

--- lib/sugarcube/fixnum.rb
+++ lib/sugarcube/fixnum.rb
@@ -6,9 +6,9 @@ class Fixnum
   # =>
   #     UIColor.colorWithRed(1, green:1, red: 1, alpha:1)
   def uicolor
-    red = ((rgb_value & 0xFF0000) >> 16).to_f / 255.0
-    green = ((rgb_value & 0xFF00) >> 8).to_f / 255.0
-    blue = (rgb_value & 0xFF).to_f / 255.0
+    red = ((self & 0xFF0000) >> 16).to_f / 255.0
+    green = ((self & 0xFF00) >> 8).to_f / 255.0
+    blue = (self & 0xFF).to_f / 255.0
 
     UIColor.colorWithRed(red, green:green, blue:blue, alpha:1.0)
   end
