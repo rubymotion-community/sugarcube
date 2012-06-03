da3650d2f35a00bbadadb7cf39d3b16569f7afb8
Colin Thomas-Arnold <colinta@gmail.com>
2012-06-03 10:05:48 -0600

    fixed uiview to_s

--- lib/sugarcube/uiview.rb
+++ lib/sugarcube/uiview.rb
@@ -18,7 +18,7 @@ class UIView
   def to_s
     "{#{self.class.name} @ x: #{self.frame.origin.x} y:#{self.frame.origin.y}, "\
                         "#{self.frame.size.width}×#{self.frame.size.height}}"\
-                        "#{self.superview ? 'child of ' + self.superview.class.name : ''}"
+                        "#{self.superview ? ' child of ' + self.superview.class.name : ''}"
   end
 
 end
