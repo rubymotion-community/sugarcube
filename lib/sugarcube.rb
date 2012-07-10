39508009b005b46d4e12cbc394d0c1db9921b25f
Colin Thomas-Arnold <colinta@gmail.com>
2012-07-10 07:51:23 -0600

    add files in order

--- lib/sugarcube.rb
+++ lib/sugarcube.rb
@@ -4,7 +4,7 @@ end
 
 
 Motion::Project::App.setup do |app|
-  Dir.glob(File.join(File.dirname(__FILE__), 'sugarcube/**/*.rb')).each do |file|
+  Dir.glob(File.join(File.dirname(__FILE__), 'sugarcube/**/*.rb')).reverse.each do |file|
     app.files.unshift(file)
   end
 end
--- lib/sugarcube/version.rb
+++ lib/sugarcube/version.rb
@@ -1,3 +1,3 @@
 module SugarCube
-  Version = '0.1.3'
+  Version = '0.1.4'
 end
