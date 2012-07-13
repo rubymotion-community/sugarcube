db4445cd0fe01cfc5784fe231a46bfd779bcf5fe
Colin Thomas-Arnold <colin@fusionbox.com>
2012-07-13 08:29:21 -0600

    added 'fadeout' method animation method

--- lib/sugarcube/uiview.rb
+++ lib/sugarcube/uiview.rb
@@ -6,19 +6,20 @@ class UIView
     self.addSubview view
   end
 
-  # i like this idea, but view[0] = view doesn't *look* like an insertion,
-  # it lookes like a replacement... it should be written as
-  #     view[0] += sub_view
-  # or
-  #     view[0] << sub_view
-  # def []=(index, view)
-  #   self.insertSubview(view, atIndex:index)
-  # end
-
   def to_s
     "{#{self.class.name} @ x: #{self.frame.origin.x} y:#{self.frame.origin.y}, "\
                         "#{self.frame.size.width}×#{self.frame.size.height}}"\
                         "#{self.superview ? ' child of ' + self.superview.class.name : ''}"
+  end
+
+  def fadeout(duration=0.3, options={}, &after)
+    UIView.animateWithDuration(duration,
+                         delay: options[:delay] || 0,
+                       options: options[:options] || UIViewAnimationOptionCurveLinear,
+                    animations: proc{
+                                  self.layer.opacity = options[:opacity] || 0
+                                }, completion:after
+                              )
   end
 
 end
