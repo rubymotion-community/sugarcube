2008e5c2065ad54afe21bb7d8ef0c49472206133
Colin Thomas-Arnold <colin@fusionbox.com>
2012-07-14 10:03:12 -0600

    fix insert_point

--- lib/sweettea.rb
+++ lib/sweettea.rb
@@ -5,19 +5,38 @@ end
 
 Motion::Project::App.setup do |app|
   # scans app.files until it finds /sugarcube/
-  insert_point = 0
+  sugarcube_insert_point = 0
   app.files.each_index do |index|
     file = app.files[index]
     if file =~ /sugarcube/
-      insert_point = index + 1
-    elsif insert_point
+      sugarcube_insert_point = index + 1
+    elsif sugarcube_insert_point
       # found one, so stop looking
       break
     end
   end
-  if not insert_point
+
+  if not sugarcube_insert_point
     raise "sweettea must be required after sugarcube."
   end
+
+  # scans app.files until it finds /teacup/
+  teacup_insert_point = 0
+  app.files.each_index do |index|
+    file = app.files[index]
+    if file =~ /teacup/
+      teacup_insert_point = index + 1
+    elsif teacup_insert_point
+      # found one, so stop looking
+      break
+    end
+  end
+
+  if not teacup_insert_point
+    raise "sweettea must be required after teacup."
+  end
+
+  insert_point = sugarcube_insert_point > teacup_insert_point ? sugarcube_insert_point : teacup_insert_point
 
   app.files.insert(insert_point, File.join(File.dirname(__FILE__), 'sweettea/handlers.rb'))
 end
