d9f3c945996e2594111fbd91ab5354ea53f7f3a9
Colin Thomas-Arnold <colin@fusionbox.com>
2012-07-15 15:06:23 -0600

    update for teacup

--- lib/sweettea/handlers.rb
+++ lib/sweettea/handlers.rb
@@ -1,40 +1,44 @@
 
-UIImageView.teacup_assign :image { |view, image|
+UIView.teacup_handler :backgroundColor { |view, color|
+  view.backgroundColor = color.uicolor
+}
+
+UIImageView.teacup_handler :image { |view, image|
   if SugarCube::CoreGraphics::Size(view.frame.size) == [0, 0]
     view.frame = SugarCube::CoreGraphics::Rect(view.frame.origin, image.size)
   end
   view.image = image.uiimage
 }
 
-UIButton.teacup_assign :normal { |view, image|
+UIButton.teacup_handler :normal { |view, image|
   if SugarCube::CoreGraphics::Size(view.frame.size) == [0, 0]
     view.frame = SugarCube::CoreGraphics::Rect(view.frame.origin, image.size)
   end
   view.setImage(image.uiimage, forState: UIControlStateNormal)
 }
 
-UIButton.teacup_assign :highlighted { |view, image|
+UIButton.teacup_handler :highlighted { |view, image|
   if SugarCube::CoreGraphics::Size(view.frame.size) == [0, 0]
     view.frame = SugarCube::CoreGraphics::Rect(view.frame.origin, image.size)
   end
   view.setImage(image.uiimage, forState: UIControlStateHighlighted)
 }
 
-UIButton.teacup_assign :bg_normal { |view, image|
+UIButton.teacup_handler :bg_normal { |view, image|
   if SugarCube::CoreGraphics::Size(view.frame.size) == [0, 0]
     view.frame = SugarCube::CoreGraphics::Rect(view.frame.origin, image.size)
   end
   view.setBackgroundImage(image.uiimage, forState: UIControlStateNormal)
 }
 
-UIButton.teacup_assign :bg_highlighted { |view, image|
+UIButton.teacup_handler :bg_highlighted { |view, image|
   if SugarCube::CoreGraphics::Size(view.frame.size) == [0, 0]
     view.frame = SugarCube::CoreGraphics::Rect(view.frame.origin, image.size)
   end
   view.setBackgroundImage(image.uiimage, forState: UIControlStateHighlighted)
 }
 
-UIButton.teacup_assign :returnKeyType, :returnkey { |view, type|
+UIButton.teacup_handler :returnKeyType, :returnkey { |view, type|
   type = type.uireturnkey if Symbol === type
   view.setReturnKeyType(type)
 }
