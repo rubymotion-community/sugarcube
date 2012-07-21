a8e055542d37a1dc14daa6317b858c9e53a4d6fd
Colin Thomas-Arnold <colin@fusionbox.com>
2012-07-21 08:42:04 -0600

    refactor uiview methods

--- lib/sugarcube/uiview.rb
+++ lib/sugarcube/uiview.rb
@@ -20,7 +20,29 @@ class UIView
     self
   end
 
-  def fade_out(duration=0.3, options={}, &after)
+  def _after_proc(after)
+    after ? proc  { |finished|
+                    if after.arity == 0
+                      after.call
+                    else
+                      after.call(finished)
+                    end
+                  } : nil
+  end
+
+  # If options is a Numeric, it is used as the duration.  Otherwise, duration
+  # is an option, and defaults to 0.3.  All the transition methods work this
+  # way.
+  def fade_out(options={}, &after)
+    if options.is_a? Numeric
+      duration = options
+      options = {}
+    else
+      duration = options[:duration] || 0.3
+    end
+
+    after = _after_proc(after)
+
     UIView.animateWithDuration(duration,
                          delay: options[:delay] || 0,
                        options: options[:options] || UIViewAnimationOptionCurveLinear,
@@ -31,48 +53,59 @@ class UIView
     self
   end
 
-  def move_to(position, duration=0.3, options={}, &after)
-    UIView.animateWithDuration(duration,
-                         delay: options[:delay] || 0,
-                       options: options[:options] || UIViewAnimationOptionCurveLinear,
-                    animations: proc{
-                                  f = self.frame
-                                  f.origin = position
-                                  self.frame = position
-                                }, completion:after
-                              )
-    self
+  def fade_in(options={}, &after)
+    if options.is_a? Numeric
+      duration = options
+      options = {}
+    else
+      duration = options[:duration] || 0.3
+    end
+
+    options[:opacity] = 1.0
+    fade_out(options, &after)
   end
 
-  def delta_to(delta, duration=0.3, options={}, &after)
-    f = self.frame
-    delta = SugarCube::CoreGraphics::Point(delta)
-    position = SugarCube::CoreGraphics::Point(f.origin)
-    position.x += delta.x
-    position.y += delta.y
+  def move_to(position, options={}, &after)
+    if options.is_a? Numeric
+      duration = options
+      options = {}
+    else
+      duration = options[:duration] || 0.3
+    end
+
+    after = _after_proc(after)
+
     UIView.animateWithDuration(duration,
                          delay: options[:delay] || 0,
                        options: options[:options] || UIViewAnimationOptionCurveLinear,
                     animations: proc{
                                   f = self.frame
-                                  f.origin = position
+                                  f.origin = SugarCube::CoreGraphics::Point(position)
                                   self.frame = f
                                 }, completion:after
                               )
     self
   end
 
-  def slide(direction, duration=0.3, options={}, &after)
+  def delta_to(delta, options={}, &after)
+    f = self.frame
+    delta = SugarCube::CoreGraphics::Point(delta)
+    position = SugarCube::CoreGraphics::Point(f.origin)
+    move_to(position + delta, options, &after)
+    self
+  end
+
+  def slide(direction, options={}, &after)
     size = UIScreen.mainScreen.bounds.size
     case direction
     when :left
-      delta_to([-size.width, 0], duration, options, &after)
+      delta_to([-size.width, 0], options, &after)
     when :right
-      delta_to([+size.width, 0], duration, options, &after)
+      delta_to([+size.width, 0], options, &after)
     when :up
-      delta_to([0, -size.height], duration, options, &after)
+      delta_to([0, -size.height], options, &after)
     when :down
-      delta_to([0, +size.height], duration, options, &after)
+      delta_to([0, +size.height], options, &after)
     else
       raise "Unknown direction #{direction.inspect}"
     end
@@ -80,9 +113,16 @@ class UIView
   end
 
   def shake(options={})
+    if options.is_a? Numeric
+      duration = options
+      options = {}
+    else
+      duration = options[:duration] || 0.3
+    end
+
     offset = options[:offset] || 8
     repeat = options[:repeat] || 3
-    duration = (options[:duration] || 0.3) / repeat
+    duration /= repeat
     keypath = options[:keypath] || 'transform.translation.x'
 
     origin = 0
@@ -95,6 +135,7 @@ class UIView
     animation.values = [origin, left, right, origin]
     animation.keyTimes = [0, 0.25, 0.75, 1.0]
     self.layer.addAnimation(animation, forKey:'shake')
+    self
   end
 
 end
