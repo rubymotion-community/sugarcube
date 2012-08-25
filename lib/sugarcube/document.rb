8da2723e7ac2211eba8ed6a72a0289fda1352ca5
Colin Thomas-Arnold <colin@fusionbox.com>
2012-08-25 10:07:12 -0600

    nsstring - document additions are smarter about always returning a document path

--- lib/sugarcube/document.rb
+++ lib/sugarcube/document.rb
@@ -1,12 +1,14 @@
 class NSString
 
   def document
-    @docs ||= NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true)[0]
-    @docs.stringByAppendingPathComponent(self)
+    @@docs ||= NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true)[0]
+    return self if self.hasPrefix(@@docs)
+
+    @@docs.stringByAppendingPathComponent(self)
   end
 
   def exists?
-    NSFileManager.defaultManager.fileExistsAtPath(self)
+    NSFileManager.defaultManager.fileExistsAtPath(self.document)
   end
 
 end
