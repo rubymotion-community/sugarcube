cc714bf3ea2f87ea8ca54d2f050a50b0d980575a
Colin Thomas-Arnold <colin@fusionbox.com>
2012-08-26 12:19:44 -0600

    to_s fix

--- lib/sugarcube/to_s/uiview.rb
+++ lib/sugarcube/to_s/uiview.rb
@@ -1,7 +1,7 @@
 class UIView
 
   def to_s
-    "#{self.class.name}(##{self.object_id}, #{self.frame.inspect}, "\
+    "#{self.class.name}(##{self.object_id}, [[#{self.frame.origin.x}, #{self.frame.origin.y}],{#{self.frame.size.width} × #{self.frame.size.height}}], "\
                         "#{self.superview ? ' child of ' + self.superview.class.name + ' #' + self.superview.object_id.to_s : ''})"
   end
 
