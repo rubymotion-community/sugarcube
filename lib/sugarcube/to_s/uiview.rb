be98495a49ce9dc6c2e1291ce4852ffe149133cd
Colin Thomas-Arnold <colinta@gmail.com>
2012-08-30 12:28:17 -0600

    UIView#to_s fix

--- lib/sugarcube/to_s/uiview.rb
+++ lib/sugarcube/to_s/uiview.rb
@@ -2,7 +2,7 @@ class UIView
 
   def to_s
     "#{self.class.name}(##{self.object_id.to_s(16)}, #{self.frame}, " +
-                        self.superview ? " child of #{self.superview.class.name}(##{self.superview.object_id.to_s(16)})" : ''
+                        (self.superview ? " child of #{self.superview.class.name}(##{self.superview.object_id.to_s(16)})" : '')
   end
 
 end
