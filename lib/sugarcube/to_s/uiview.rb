861b0367036675204eed91276b4d86d8214784ef
Colin Thomas-Arnold <colinta@gmail.com>
2012-09-28 11:47:27 -0600

    to_s fix in UIView

--- lib/sugarcube/to_s/uiview.rb
+++ lib/sugarcube/to_s/uiview.rb
@@ -1,8 +1,8 @@
 class UIView
 
   def to_s
-    "#{self.class.name}(##{self.object_id.to_s(16)}, #{SugarCube::Adjust::format_frame(self.frame)}, " +
-                        (self.superview ? " child of #{self.superview.class.name}(##{self.superview.object_id.to_s(16)})" : '')
+    "#{self.class.name}(##{self.object_id.to_s(16)}, #{SugarCube::Adjust::format_frame(self.frame)})" +
+                        (self.superview ? ",  child of #{self.superview.class.name}(##{self.superview.object_id.to_s(16)})" : '')
   end
 
 end
