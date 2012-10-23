1038287165a2e2d42fffcbf91b12a8dd9081c712
Colin Thomas-Arnold <colinta@gmail.com>
2012-10-23 17:45:34 -0600

    UIImage manipulation methods

--- lib/sugarcube/uiimage.rb
+++ lib/sugarcube/uiimage.rb
@@ -14,5 +14,26 @@ class UIImage
     @uiimageview = UIImageView.alloc.initWithImage(self)
   end
 
+  ##|
+  ##|  REALLY HANDY STUFF!
+  ##|
+  def scale_to new_size
+    UIGraphicsBeginImageContextWithOptions(new_size, false, 0.0)
+    self.drawInRect([[0, 0], new_size])
+    image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return image
+  end
+
+  def rounded(corner_radius=5)
+    UIGraphicsBeginImageContext(size)
+    path = UIBezierPath.bezierPathWithRoundedRect([[0, 0], size], cornerRadius:corner_radius)
+    path.addClip
+    self.drawInRect([[0, 0], size])
+    image = UIGraphicsGetImageFromCurrentImageContext()
+
+    UIGraphicsEndImageContext()
+    return image
+  end
 
 end
