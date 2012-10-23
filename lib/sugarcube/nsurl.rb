4fbb06ced6cbb0b3c438aaae2b338f005056b334
Colin Thomas-Arnold <colinta@gmail.com>
2012-10-23 10:53:47 -0600

    nsurl += can_open?

--- lib/sugarcube/nsurl.rb
+++ lib/sugarcube/nsurl.rb
@@ -4,4 +4,8 @@ class NSURL
     NSUIApplication.sharedApplication.openURL(NSUrl.URLWithString(self))
   end
 
+  def can_open?
+    NSUIApplication.sharedApplication.canOpenURL(NSUrl.URLWithString(self))
+  end
+
 end
