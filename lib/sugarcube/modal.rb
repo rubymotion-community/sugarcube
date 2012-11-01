b9c5c703f2aa44d69affb351073960e3d733ffad
Colin Thomas-Arnold <colinta@gmail.com>
2012-11-01 11:32:02 -0600

    present modal in nav - makes it easy to have a cancel button

--- lib/sugarcube/modal.rb
+++ lib/sugarcube/modal.rb
@@ -5,6 +5,14 @@ module SugarCube
       UIApplication.sharedApplication.keyWindow.rootViewController.presentViewController(view_ctlr, animated:true, completion:block)
     end
 
+    def present_modal_in_nav(view_ctlr, &block)
+      ctlr = UINavigationController.alloc.initWithRootViewController(view_ctlr)
+      ctlr.modalTransitionStyle = UIModalTransitionStyleCoverVertical
+
+      SugarCube::Modal.present_modal(ctlr, &block)
+      ctlr
+    end
+
     def dismiss_modal(&block)
       UIApplication.sharedApplication.keyWindow.rootViewController.dismissViewControllerAnimated(true, completion:block)
     end
