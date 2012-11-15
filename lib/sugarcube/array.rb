174ef44c199b4bcdc39720e56acd9df1dd61f190
Colin Thomas Arnold Gray <colinta@gmail.com>
2012-11-15 11:38:12 -0700

    playing with yard

--- lib/sugarcube/array.rb
+++ lib/sugarcube/array.rb
@@ -1,5 +1,7 @@
 class Array
 
+  # @param [Symbol] type A pointer type from the list at {http://www.rubymotion.com/developer-center/guides/runtime/ RubyMotion Pointers Reference#_pointers}
+  # @return [Pointer] A pointer to the array, of the specified type
   def to_pointer(type)
     ret = Pointer.new(type, self.length)
     self.each_index do |i|
@@ -8,6 +10,8 @@ class Array
     ret
   end
 
+  # Creates an NSIndexPath object using the items in `self` as the indices
+  # @return [NSIndexPath]
   def nsindexpath
     if self.length == 0
       raise "An index path must have at least one index"
@@ -24,6 +28,8 @@ class Array
     return path
   end
 
+  # Creates an NSIndexSet object using the items in `self` as the indices
+  # @return [NSIndexSet]
   def nsindexset
     if self.length == 0
       raise "An index set must have at least one index"
