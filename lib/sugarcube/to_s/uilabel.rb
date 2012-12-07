83d265b09089465c2c2442d154eafc7c434f0f39
Colin Thomas Arnold Gray <colinta@gmail.com>
2012-12-07 11:31:19 -0700

    truncate uilabel if it's long

--- lib/sugarcube/to_s/uilabel.rb
+++ lib/sugarcube/to_s/uilabel.rb
@@ -1,6 +1,10 @@
 class UILabel
 
   def to_s(options={})
+    text = self.text
+    if text && text.length > 20
+      text = text[0..20] + '...'
+    end
     super options.merge(inner: 'text: ' + text.inspect)
   end
 
