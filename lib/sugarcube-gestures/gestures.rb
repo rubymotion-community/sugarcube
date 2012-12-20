be96c6055178e59b6751ef4b65b5866eaa6339fd
Colin Thomas Arnold Gray <colinta@gmail.com>
2012-12-20 07:22:09 -0700

    added 'gestures' to README, and changed case to match that of UIControl 'on' handlers

--- lib/sugarcube-gestures/gestures.rb
+++ lib/sugarcube-gestures/gestures.rb
@@ -3,14 +3,14 @@
 # changing it to suit my needs, and offering it here
 class UIView
 
-  def on_tapped(taps_or_options=nil, &proc)
+  def on_tap(taps_or_options=nil, &proc)
     taps = nil
-    touches = nil
+    fingers = nil
 
     if taps_or_options
       if taps_or_options.is_a? Hash
         taps = taps_or_options[:taps] || taps
-        touches = taps_or_options[:touches] || touches
+        fingers = taps_or_options[:fingers] || fingers
       else
         taps = taps_or_options
       end
@@ -18,28 +18,28 @@ class UIView
 
     recognizer = UITapGestureRecognizer.alloc.initWithTarget(self, action:'sugarcube_handle_gesture:')
     recognizer.numberOfTapsRequired = taps if taps
-    recognizer.numberOfTouchesRequired = touches if touches
+    recognizer.numberOfTouchesRequired = fingers if fingers
     sugarcube_add_gesture(proc, recognizer)
   end
 
-  def on_pinched(direction_or_options=nil, &proc)
+  def on_pinch(&proc)
     recognizer = UIPinchGestureRecognizer.alloc.initWithTarget(self, action:'sugarcube_handle_gesture:')
     sugarcube_add_gesture(proc, recognizer)
   end
 
-  def on_rotated(direction_or_options=nil, &proc)
+  def on_rotate(&proc)
     recognizer = UIRotationGestureRecognizer.alloc.initWithTarget(self, action:'sugarcube_handle_gesture:')
     sugarcube_add_gesture(proc, recognizer)
   end
 
-  def on_swiped(direction_or_options=nil, &proc)
+  def on_swipe(direction_or_options=nil, &proc)
     direction = UISwipeGestureRecognizerDirectionRight
-    touches = nil
+    fingers = nil
 
     if direction_or_options
       if direction_or_options.is_a? Hash
         direction = direction_or_options[:direction] || direction
-        touches = direction_or_options[:touches] || touches
+        fingers = direction_or_options[:fingers] || fingers
       else
         direction = direction_or_options
       end
@@ -58,25 +58,39 @@ class UIView
 
     recognizer = UISwipeGestureRecognizer.alloc.initWithTarget(self, action:'sugarcube_handle_gesture:')
     recognizer.direction = direction if direction
-    recognizer.numberOfTouchesRequired = touches if touches
+    recognizer.numberOfTouchesRequired = fingers if fingers
     sugarcube_add_gesture(proc, recognizer)
   end
 
-  def on_panned(direction_or_options=nil, &proc)
+  def on_pan(fingers_or_options=nil, &proc)
+    fingers = nil
+    min_fingers = nil
+    max_fingers = nil
+
+    if fingers_or_options
+      if fingers_or_options.is_a? Hash
+        fingers = fingers_or_options[:fingers] || fingers
+        min_fingers = fingers_or_options[:min_fingers] || min_fingers
+        max_fingers = fingers_or_options[:max_fingers] || max_fingers
+      else
+        fingers = fingers_or_options
+      end
+    end
+
     recognizer = UIPanGestureRecognizer.alloc.initWithTarget(self, action:'sugarcube_handle_gesture:')
     sugarcube_add_gesture(proc, recognizer)
   end
 
-  def on_pressed(duration_or_options=nil, &proc)
+  def on_press(duration_or_options=nil, &proc)
     duration = nil
     taps = nil
-    touches = nil
+    fingers = nil
 
     if duration_or_options
       if duration_or_options.is_a? Hash
         duration = duration_or_options[:duration] || duration
         taps = duration_or_options[:taps] || taps
-        touches = duration_or_options[:touches] || touches
+        fingers = duration_or_options[:fingers] || fingers
       else
         duration = duration_or_options
       end
@@ -85,12 +99,11 @@ class UIView
     recognizer = UILongPressGestureRecognizer.alloc.initWithTarget(self, action:'sugarcube_handle_gesture:')
     recognizer.minimumPressDuration = duration if duration
     recognizer.numberOfTapsRequired = taps if taps
-    recognizer.numberOfTouchesRequired = touches if touches
+    recognizer.numberOfTouchesRequired = fingers if fingers
     sugarcube_add_gesture(proc, recognizer)
   end
 
-  private
-
+private
   def sugarcube_handle_gesture(recognizer)
     handler = @sugarcube_recognizers[recognizer]
     if handler.arity == 0
