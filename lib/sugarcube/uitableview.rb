9314220ab5579526d4b37b724897c7e33650ebde
Colin Thomas Arnold Gray <colinta@gmail.com>
2012-12-19 20:27:30 -0700

    default frame should not assume iphone

--- lib/sugarcube/uitableview.rb
+++ lib/sugarcube/uitableview.rb
@@ -1,11 +1,11 @@
 class UITableView
   class << self
 
-    def plain(frame=[[0, 0], [320, 480]])
+    def plain(frame=[[0, 0], [0, 0]])
       UITableView.alloc.initWithFrame(frame, style: :plain.uitableviewstyle)
     end
 
-    def grouped(frame=[[0, 0], [320, 480]])
+    def grouped(frame=[[0, 0], [0, 0]])
       UITableView.alloc.initWithFrame(frame, style: :grouped.uitableviewstyle)
     end
 
