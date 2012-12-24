9877f47440363897dc02ba8ceccd6189488ae125
Colin Thomas Arnold Gray <colinta@gmail.com>
2012-12-24 09:29:10 -0700

    fix white/black colors not returning RGB components

--- lib/sugarcube/uicolor.rb
+++ lib/sugarcube/uicolor.rb
@@ -24,14 +24,23 @@ private
       red = Pointer.new(:float)
       green = Pointer.new(:float)
       blue = Pointer.new(:float)
+      white = Pointer.new(:float)
       alpha = Pointer.new(:float)
-      self.getRed(red, green:green, blue:blue, alpha:alpha)
-      @color = {
-        red: red[0],
-        green: green[0],
-        blue: blue[0],
-        alpha: alpha[0],
-      }
+      if self.getRed(red, green:green, blue:blue, alpha:alpha)
+        {
+          red: red[0],
+          green: green[0],
+          blue: blue[0],
+          alpha: alpha[0],
+        }
+      elsif self.getWhite(white, alpha:alpha)
+        {
+          red: white[0],
+          green: white[0],
+          blue: white[0],
+          alpha: alpha[0],
+        }
+      end
     end
   end
 
