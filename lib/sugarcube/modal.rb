e1c7976304361be111cb5e9dbaecccf78fbd3bfc
Colin Thomas Arnold Gray <colinta@gmail.com>
2012-12-26 10:55:42 -0700

    refactor 'modal' to accept options, and add methods to UIViewController to use 'self' as 'target' option

--- lib/sugarcube/modal.rb
+++ lib/sugarcube/modal.rb
@@ -1,20 +1,39 @@
 module SugarCube
   module Modal
     module_function
-    def present_modal(view_ctlr, &block)
-      UIApplication.sharedApplication.keyWindow.rootViewController.presentViewController(view_ctlr, animated:true, completion:block)
+    def present_modal(view_ctlr, target=nil, &block)
+      target ||= UIApplication.sharedApplication.keyWindow.rootViewController
+      target.presentViewController(view_ctlr, animated:true, completion:block)
     end
 
-    def present_modal_in_nav(view_ctlr, &block)
+    def present_modal_in_nav(view_ctlr, target=nil, &block)
       ctlr = UINavigationController.alloc.initWithRootViewController(view_ctlr)
       ctlr.modalTransitionStyle = UIModalTransitionStyleCoverVertical
 
-      SugarCube::Modal.present_modal(ctlr, &block)
+      SugarCube::Modal.present_modal(ctlr, target, &block)
       ctlr
     end
 
-    def dismiss_modal(&block)
-      UIApplication.sharedApplication.keyWindow.rootViewController.dismissViewControllerAnimated(true, completion:block)
+    def dismiss_modal(target=nil, &block)
+      target ||= UIApplication.sharedApplication.keyWindow.rootViewController
+      target.dismissViewControllerAnimated(true, completion:block)
     end
   end
+end
+
+
+class UIViewController
+
+  def present_modal(view_ctlr, &block)
+    SugarCube::Modal.present_modal(view_ctlr, self, &block)
+  end
+
+  def present_modal_in_nav(view_ctlr, &block)
+    SugarCube::Modal.present_modal_in_nav(view_ctlr, self, &block)
+  end
+
+  def dismiss_modal(view_ctlr, &block)
+    SugarCube::Modal.dismiss_modal(view_ctlr, self, &block)
+  end
+
 end
