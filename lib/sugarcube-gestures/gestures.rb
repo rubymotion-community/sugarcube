babf345d70a25d6cbce9eec0768ddc8cc3f7d703
Colin Thomas Arnold Gray <colinta@gmail.com>
2012-12-28 09:04:44 -0700

    documentation for gestures

--- lib/sugarcube-gestures/gestures.rb
+++ lib/sugarcube-gestures/gestures.rb
@@ -3,15 +3,39 @@
 # changing it to suit my needs, and offering it here
 class UIView
 
-  # a generic gesture adder, but using sugarcube_add_gesture, which handles the block
+  # A generic gesture adder, but accepts a block like the other gesture methods
+  # @yield [recognizer] Handles the gesture event, and passes the recognizer instance to the block.
+  # @param options [Hash] method/value pairs to call on the gesture.
+  # @overload on_gesture(recognizer)
+  #   Adds the gesture to the view, and yields the block when the gesture is recognized
+  # @overload on_gesture(recognizer_class)
+  #   Instantiates a gesture and adds it to the view.
+  # @example Using a UIGestureRecognizer class
+  #   view.on_gesture(UISwipeGestureRecognizer, direction: UISwipeGestureRecognizerDirectionLeft) { puts "swiped left" }
+  # @example Using a UIGestureRecognizer instance
+  #   gesture = UISwipeGestureRecognizer
+  #   gesture.direction = UISwipeGestureRecognizerDirectionLeft
+  #   view.on_gesture(gesture) { puts "swiped left" }
   def on_gesture(klass, options={}, &proc)
-    recognizer = klass.alloc.initWithTarget(self, action:'sugarcube_handle_gesture:')
+    if klass.is_a? UIGestureRecognizer
+      recognizer = klass
+      recognizer.addTarget(self, action:'sugarcube_handle_gesture:')
+    else
+      recognizer = klass.alloc.initWithTarget(self, action:'sugarcube_handle_gesture:')
+    end
+
     options.each do |method, value|
       recognizer.send(method, value)
     end
     sugarcube_add_gesture(proc, recognizer)
   end
 
+  # @yield [recognizer] Handles the gesture event, and passes the recognizer instance to the block.
+  # @overload on_tap(taps)
+  #   @param taps [Fixnum] Number of taps
+  # @overload on_tap(options)
+  #   @option options [Fixnum] :taps Number of taps before gesture is recognized
+  #   @option options [Fixnum] :fingers Number of fingers before gesture is recognized
   def on_tap(taps_or_options=nil, &proc)
     taps = nil
     fingers = nil
@@ -31,16 +55,24 @@ class UIView
     sugarcube_add_gesture(proc, recognizer)
   end
 
+  # @yield [recognizer] Handles the gesture event, and passes the recognizer instance to the block.
   def on_pinch(&proc)
     recognizer = UIPinchGestureRecognizer.alloc.initWithTarget(self, action:'sugarcube_handle_gesture:')
     sugarcube_add_gesture(proc, recognizer)
   end
 
+  # @yield [recognizer] Handles the gesture event, and passes the recognizer instance to the block.
   def on_rotate(&proc)
     recognizer = UIRotationGestureRecognizer.alloc.initWithTarget(self, action:'sugarcube_handle_gesture:')
     sugarcube_add_gesture(proc, recognizer)
   end
 
+  # @yield [recognizer] Handles the gesture event, and passes the recognizer instance to the block.
+  # @overload on_swipe(taps)
+  #   @param direction [Fixnum] Direction of swipe
+  # @overload on_swipe(options)
+  #   @option options [Fixnum] :fingers Number of fingers before gesture is recognized
+  #   @option options [Fixnum, Symbol] :direction Direction of swipe, as a UISwipeGestureRecognizerDirection constant or a symbol (`:left, :right, :up, :down`)
   def on_swipe(direction_or_options=nil, &proc)
     direction = UISwipeGestureRecognizerDirectionRight
     fingers = nil
@@ -71,6 +103,13 @@ class UIView
     sugarcube_add_gesture(proc, recognizer)
   end
 
+  # @yield [recognizer] Handles the gesture event, and passes the recognizer instance to the block.
+  # @overload on_tap(taps)
+  #   @param taps [Fixnum] Number of taps
+  # @overload on_tap(options)
+  #   @option options [Fixnum] :min_fingers Minimum umber of fingers for gesture to be recognized
+  #   @option options [Fixnum] :max_fingers Maximum number of fingers for gesture to be recognized
+  #   @option options [Fixnum] :fingers If min_fingers or max_fingers is not assigned, this will be the default.
   def on_pan(fingers_or_options=nil, &proc)
     fingers = nil
     min_fingers = nil
@@ -80,15 +119,29 @@ class UIView
       if fingers_or_options.is_a? Hash
         fingers = fingers_or_options[:fingers] || fingers
         min_fingers = fingers_or_options[:min_fingers] || min_fingers
-        max_fingers = fingers_or_options[:max_fingers] || max_fingers      else
+        max_fingers = fingers_or_options[:max_fingers] || max_fingers
+      else
         fingers = fingers_or_options
       end
     end
 
+    # if fingers is assigned, but not min/max, assign it as a default
+    min_fingers ||= fingers
+    max_fingers ||= fingers
+
     recognizer = UIPanGestureRecognizer.alloc.initWithTarget(self, action:'sugarcube_handle_gesture:')
+    recognizer.maximumNumberOfTouches = min_fingers if min_fingers
+    recognizer.minimumNumberOfTouches = max_fingers if max_fingers
     sugarcube_add_gesture(proc, recognizer)
   end
 
+  # @yield [recognizer] Handles the gesture event, and passes the recognizer instance to the block.
+  # @overload on_press(duration)
+  #   @param duration [Fixnum] How long in seconds before gesture is recognized
+  # @overload on_tap(options)
+  #   @option options [Fixnum] :duration How long in seconds before gesture is recognized
+  #   @option options [Fixnum] :taps Number of taps before gesture is recognized
+  #   @option options [Fixnum] :fingers Number of fingers before gesture is recognized
   def on_press(duration_or_options=nil, &proc)
     duration = nil
     taps = nil
