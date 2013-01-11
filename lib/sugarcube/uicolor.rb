fd50c7b1018d616c6605eeee1564e6adc0d3f2a6
Colin Thomas Arnold Gray <colinta@gmail.com>
2013-01-11 11:06:27 -0700

    added css_name

--- lib/sugarcube/uicolor.rb
+++ lib/sugarcube/uicolor.rb
@@ -21,7 +21,22 @@ class UIColor
     _sugarcube_colors && _sugarcube_colors[:alpha]
   end
 
+  # returns the closest css name
+  def css_name
+    found = Symbol.css_colors.map { |key, val| [key, _sugarcube_color_compare(self, val.uicolor)] }.inject{|c1,c2| c1[1] < c2[1] ? c1 : c2 }
+    threshold = 0.03
+    if found[1] > 0.03
+      nil
+    else
+      found[0]
+    end
+  end
+
 private
+  def _sugarcube_color_compare(c1, c2)
+    return (c1.red - c2.red).abs + (c1.green - c2.green).abs + (c1.blue - c2.blue).abs
+  end
+
   def _sugarcube_colors
     @color ||= begin
       red = Pointer.new(:float)
