6c350b99a0dfe12eba187d539eaa0d9311da370f
Colin Thomas Arnold Gray <colinta@gmail.com>
2013-01-11 11:06:44 -0700

    fixes to UIColor#to_s

--- lib/sugarcube/to_s/uicolor.rb
+++ lib/sugarcube/to_s/uicolor.rb
@@ -3,7 +3,7 @@ class UIColor
     system_color = nil
     Symbol.uicolors.each_pair do |color, method|
       if UIColor.send(method) == self
-        if self.alpha < 1
+        if self.alpha && self.alpha < 1
           system_color = "UIColor.#{method}(#{alpha})"
         else
           system_color = "UIColor.#{method}"
@@ -20,7 +20,7 @@ class UIColor
       my_color = red + green + blue
 
       inside = my_color.to_s(16)
-      inside = '0x' + '0' * (6 - inside.length)
+      inside = '0x' + '0' * (6 - inside.length) + inside
 
       Symbol.css_colors.each_pair do |color, hex|
         if hex == my_color
