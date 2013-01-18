693ab0e3049717f7f3d8220bbbe6fb0a459c7053
Colin Thomas Arnold Gray <colinta@gmail.com>
2013-01-18 08:31:20 -0700

    more tests

--- spec/symbol_uicolor_spec.rb
+++ spec/symbol_uicolor_spec.rb
@@ -1,10 +1,24 @@
 describe 'Symbol#uicolor' do
 
   it "should return css color names" do
+    except_for = [:aqua, :fuchsia, :lime]
     Symbol.css_colors.each do |name, val|
+      next if except_for.include? name
+
       color = val.uicolor
       color.to_s.should == "UIColor.color(#{name.inspect})"
     end
   end
+
+  it "should return UIColor objects" do
+    Symbol.uicolors.each do |name, method|
+      name.uicolor.should == UIColor.send(method)
+    end
+    Symbol.css_colors.each do |name, val|
+      name.uicolor.is_a?(UIColor).should == true
+    end
+  end
+
+
 
 end
