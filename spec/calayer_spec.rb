a8242a846a54065ac908986d1b4909eac5fa71f2
Colin Thomas Arnold Gray <colinta@gmail.com>
2013-01-18 08:31:20 -0700

    more tests

---
+++ spec/calayer_spec.rb
@@ -0,0 +1,10 @@
+describe "CALayer" do
+
+  it "should support <<" do
+    layer = CALayer.layer
+    another_layer = CALayer.layer
+    layer << another_layer
+    layer.sublayers.should == [another_layer]
+  end
+
+end
