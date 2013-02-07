6ce43f29559b183596ad0ee1879831e0b5a8c159
Colin Thomas Arnold Gray <colinta@gmail.com>
2013-02-06 17:14:16 -0700

    NSURL spec

---
+++ spec/nsurl_spec.rb
@@ -0,0 +1,11 @@
+describe "NSURL" do
+
+  it "should have a method #open" do
+    'test'.nsurl.respond_to?(:open).should == true
+  end
+
+  it "should have a method #can_open?" do
+    'https://github.com'.nsurl.can_open?.should == true
+  end
+
+end
