3f656928b6d46d2323d1903f0433705acb07db8b
Colin Thomas Arnold Gray <colinta@gmail.com>
2013-02-12 10:29:27 -0700

    testing UIView#tumble

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,4 +1,26 @@
 include SugarCube::CoreGraphics
 
 class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    return true if RUBYMOTION_ENV == 'test'
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    ctlr = MyController.new
+    @window.rootViewController = ctlr
+    @window.makeKeyAndVisible
+  end
+end
+
+
+class MyController < UIViewController
+
+  def loadView
+    super.tap do
+      @label = 'Hi!'.uilabel
+      @label.center = self.view.center
+      @label.textColor = :white.uicolor
+      self.view << @label
+    end
+  end
+
 end
