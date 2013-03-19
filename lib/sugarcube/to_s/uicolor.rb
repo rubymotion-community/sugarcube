c6c33ab14f6957ce5e58695c5641486c1e0b0aa4
Colin T.A. Gray <colinta@gmail.com>
2013-03-19 09:56:35 -0600

    more refactors to UIColor#to_s - added inspect and to_system methods, keeps to_s and inspect very short

--- lib/sugarcube/to_s/uicolor.rb
+++ lib/sugarcube/to_s/uicolor.rb
@@ -12,14 +12,27 @@ class UIColor
   end
 
   def to_hex
-    if my_color = self.to_i
+    my_color = self.to_i
+    if my_color
       return '#' + my_color.to_s(16).rjust(6, '0')
     else
       nil
     end
   end
 
-  def to_s
+  def to_css
+    my_color = self.to_i
+    css_name = nil
+    Symbol.css_colors.each_pair do |color, hex|
+      if hex == my_color
+        css_name = color.inspect
+        break
+      end
+    end
+    return css_name
+  end
+
+  def to_system
     system_color = nil
     Symbol.uicolors.each_pair do |color, method|
       if UIColor.send(method) == self
@@ -31,23 +44,33 @@ class UIColor
         break
       end
     end
+    return system_color
+  end
+
+  def to_s
+    system_color = to_system
     return system_color if system_color
 
-    inside = self.to_hex
+    inside = self.to_css || self.to_hex
     if inside
-      my_color = self.to_i
-      Symbol.css_colors.each_pair do |color, hex|
-        if hex == my_color
-          inside = color.inspect
-          break
-        end
-      end
-
       if self.alpha < 1
         return "UIColor.color(#{inside}, #{alpha})"
       else
         return "UIColor.color(#{inside})"
       end
+    else
+      super
+    end
+  end
+
+  def inspect
+    alpha_s = (alpha < 1 ? "(#{alpha})" : '')
+    if to_system
+      return to_system
+    elsif to_hex
+      return "'#{to_hex}'.uicolor#{alpha_s}"
+    elsif to_css
+      return ":#{to_css}.uicolor#{alpha_s}"
     else
       super
     end
