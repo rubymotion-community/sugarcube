c183c30004cf09749fd110e7354ce1f706fd3841
Colin T.A. Gray <colinta@gmail.com>
2013-03-21 07:20:00 -0600

    changed Katsuyoshi's 'NSDictionary' methods so that they are now in a subclass, `SugarCube::Struct`

---
+++ lib/sugarcube/struct.rb
@@ -0,0 +1,46 @@
+module SugarCube
+  # A subclass of Hash that allows its keys to be accessed and assigned by
+  # method name.  Useful to quickly mock objects.  To that end, a
+  # `NoMethodError` is raised anytime the key doesn't exist (getter or setter)
+  #
+  # You can convert an existing Hash or NSDictionary into a Struct using the
+  # constructor `SugarCube::Struct[hash]`, which I recommend you define as a
+  # function.
+  #
+  # @example
+  #     # in app_delegate.rb
+  #     ::Struct = SugarCube::Struct
+  #     def Struct(hash) ; SugarCube.Struct[hash] ; end
+  #
+  #     obj = Struct({
+  #       'first_name': 'Katsuyoshi',  # strings
+  #       company: 'Ito Soft Design',  # and symbols are supported
+  #     })
+  #     obj.first_name
+  #     # => 'Katsuyoshi'
+  #     obj.company
+  #     # => 'Ito Soft Design'
+  #     obj.bla  # => raises NoMethodError
+  #     obj.bla = 'bla'  # => raises NoMethodError
+  class Struct < Hash
+
+    def method_missing(symbol, *args)
+      if args.size == 0
+        key = symbol
+        key = symbol.to_s unless self.include? key
+        if self.include?(key)
+          return self[key]
+        end
+      elsif args.size == 1 && /(.*)=$/ =~ symbol.to_s
+        key = $1.to_sym
+        key = key.to_s unless self.include? key
+        if self.include?(key)
+          return self[key] = args.first
+        end
+      end
+      return super
+    end
+
+  end
+
+end
---
+++ spec/struct_spec.rb
@@ -0,0 +1,53 @@
+describe "SugarCube::Struct" do
+
+  before do
+    @h = SugarCube::Struct[{ foo: 'FOO', "bar" => 'BAR' }]
+  end
+
+  describe "constructor" do
+
+    it 'should give an instance of Struct' do
+      SugarCube::Struct.should === @h
+    end
+
+  end
+
+  describe "getter" do
+
+    it 'should have an #foo method, and return "FOO"' do
+      @h.foo.should == 'FOO'
+    end
+
+    it 'should have an #bar method, and return "BAR"' do
+      @h.bar.should == 'BAR'
+    end
+
+    it 'should raise NoMethodError on non-existing keys' do
+      should.raise(NoMethodError) {
+        @h.hoge
+      }
+    end
+
+  end
+
+  describe "setter" do
+
+    it 'should have an #foo= method, and return "Foo"' do
+      @h.foo = 'Foo'
+      @h.foo.should == 'Foo'
+    end
+
+    it 'should have a #bar= method, and return "Bar"' do
+      @h.bar = 'Bar'
+      @h.bar.should == 'Bar'
+    end
+
+    it 'should raise NoMethodError on non-existing keys' do
+      should.raise(NoMethodError) {
+        @h.hoge.should == 'Hoge'
+      }
+    end
+
+  end
+
+end
