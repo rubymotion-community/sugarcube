9afe8afa80cfad9d5d52d52a92d617652e3b42d9
Colin Thomas Arnold Gray <colinta@gmail.com>
2013-03-20 18:05:19 -0600

    oops! quick fix!

--- lib/sugarcube/to_s/nsurl.rb
+++ lib/sugarcube/to_s/nsurl.rb
@@ -1,7 +1,13 @@
-class NSError
+class NSURL
+
   def to_s
+    self.absoluteString
+  end
+
+  def inspect
     "#<#{self.class.name}:0x#{self.object_id.to_s(16)}, "+
       "URL=#{self.absoluteString.inspect}"+
      ">"
   end
+
 end
