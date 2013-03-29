7be0f74fb7027d86613302b74b5df5132d28c9d4
Katsuyoshi Ito <kito@itosoft.com>
2013-03-29 10:17:12 +0900

    remove debug print

--- lib/sugarcube/symbol/symbol_uicolor.rb
+++ lib/sugarcube/symbol/symbol_uicolor.rb
@@ -20,18 +20,6 @@ class Symbol
     color
   end
 
-p "*"*(80-2), Hash[*UIColor.methods.
-           select{ |x| x =~/Color$/ }.
-           map{ |x|
-             word = x.to_s[0...-5]
-             word.gsub!(/::/, '/')
-             word.gsub!(/([A-Z\d]+)([A-Z][a-z])/,'\1_\2')
-             word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
-             word.tr!("-", "_")
-             word.downcase!
-             [word.to_sym, x]
-           }.flatten]
-
   @uicolors = {
     table_view:  :groupTableViewBackgroundColor,
     scroll_view: :scrollViewTexturedBackgroundColor,
