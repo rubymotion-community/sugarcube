0edd9fd96749e62874537ef24a12c4bdc9c9d9f3
Colin T.A. Gray <colinta@gmail.com>
2013-04-19 08:49:19 -0600

    added 'sugarcube-repl' to make helper methods 'object-level', but private so they shouldn't be quite so easily called accidentally

---
+++ lib/sugarcube-repl/object.rb
@@ -0,0 +1,97 @@
+class Object
+
+private
+  def adjust(*args)
+    SugarCube::Adjust.adjust(*args)
+  end
+  def a(*args)
+    SugarCube::Adjust.adjust(*args)
+  end
+  def frame(*args)
+    SugarCube::Adjust.frame(*args)
+  end
+  def f(*args)
+    SugarCube::Adjust.frame(*args)
+  end
+  def left(*args)
+    SugarCube::Adjust.left(*args)
+  end
+  def l(*args)
+    SugarCube::Adjust.left(*args)
+  end
+  def right(*args)
+    SugarCube::Adjust.right(*args)
+  end
+  def r(*args)
+    SugarCube::Adjust.right(*args)
+  end
+  def up(*args)
+    SugarCube::Adjust.up(*args)
+  end
+  def u(*args)
+    SugarCube::Adjust.up(*args)
+  end
+  def down(*args)
+    SugarCube::Adjust.down(*args)
+  end
+  def d(*args)
+    SugarCube::Adjust.down(*args)
+  end
+  def origin(*args)
+    SugarCube::Adjust.origin(*args)
+  end
+  def o(*args)
+    SugarCube::Adjust.origin(*args)
+  end
+  def thinner(*args)
+    SugarCube::Adjust.thinner(*args)
+  end
+  def n(*args)
+    SugarCube::Adjust.thinner(*args)
+  end
+  def wider(*args)
+    SugarCube::Adjust.wider(*args)
+  end
+  def w(*args)
+    SugarCube::Adjust.wider(*args)
+  end
+  def shorter(*args)
+    SugarCube::Adjust.shorter(*args)
+  end
+  def s(*args)
+    SugarCube::Adjust.shorter(*args)
+  end
+  def taller(*args)
+    SugarCube::Adjust.taller(*args)
+  end
+  def t(*args)
+    SugarCube::Adjust.taller(*args)
+  end
+  def size(*args)
+    SugarCube::Adjust.size(*args)
+  end
+  def z(*args)
+    SugarCube::Adjust.size(*args)
+  end
+  def center(*args)
+    SugarCube::Adjust.center(*args)
+  end
+  def c(*args)
+    SugarCube::Adjust.center(*args)
+  end
+  def shadow(*args)
+    SugarCube::Adjust.shadow(*args)
+  end
+  def h(*args)
+    SugarCube::Adjust.shadow(*args)
+  end
+  def tree(*args)
+    SugarCube::Adjust.tree(*args)
+  end
+  def root(*args)
+    SugarCube::Adjust.root(*args)
+  end
+  def restore(*args)
+    SugarCube::Adjust.restore(*args)
+  end
+end
