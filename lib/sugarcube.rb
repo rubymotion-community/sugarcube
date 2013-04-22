d917350d5495e58bfb5374623e3d977825488b52
Colin T.A. Gray <colinta@gmail.com>
2013-04-22 14:18:10 -0600

    uses new shorter app/ index finder

--- lib/sugarcube.rb
+++ lib/sugarcube.rb
@@ -7,15 +7,7 @@ Motion::Project::App.setup do |app|
   # scans app.files until it finds app/ (the default)
   # if found, it inserts just before those files, otherwise it will insert to
   # the end of the list
-  insert_point = 0
-  app.files.each_index do |index|
-    file = app.files[index]
-    if file =~ /^(?:\.\/)?app\//
-      # found app/, so stop looking
-      break
-    end
-    insert_point = index + 1
-  end
+  insert_point = app.files.find_index { |file| file =~ /^(?:\.\/)?app\// } || 0
 
   Dir.glob(File.join(File.dirname(__FILE__), 'sugarcube/**/*.rb')).reverse.each do |file|
     app.files.insert(insert_point, file)
