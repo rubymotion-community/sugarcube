9583cdb00611d5fe4f10ab3356b919d4adf1f287
Katsuyoshi Ito <kito@itosoft.com>
2013-04-25 00:29:27 +0900

    get a timezone from a name

---
+++ spec/nsstring_timezone_spec.rb
@@ -0,0 +1,23 @@
+describe "NSString to NSTimeZone" do
+
+  it "should parse timezone as UTC" do
+    "UTC".nstimezone.secondsFromGMT.should == 0
+  end
+  
+  it "should parse timezone as Asia/Tokyo" do
+    "Asia/Tokyo".nstimezone.secondsFromGMT.should == 9 * 60 * 60
+  end
+  
+  it "should parse timezone as Asia/Tokyo" do
+    "UTC+9".nstimezone.secondsFromGMT.should == 9 * 60 * 60
+  end
+  
+  it "should parse timezone as Asia/Tokyo" do
+    "GMT+9".nstimezone.secondsFromGMT.should == 9 * 60 * 60
+  end
+  
+  it "should parse timezone as Asia/Tokyo" do
+    "+0900".nstimezone.secondsFromGMT.should == 9 * 60 * 60
+  end
+  
+end
