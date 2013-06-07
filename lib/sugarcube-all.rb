681b682b73328b1c4f390ced5c1bccd77b47d53f
Colin T.A. Gray <colinta@gmail.com>
2013-06-07 17:15:57 -0600

    it's long overdue

--- lib/sugarcube-all.rb
+++ lib/sugarcube-all.rb
@@ -1,4 +1,11 @@
-ignore = ['sugarcube-all.rb']
+ignore = [
+  'sugarcube-all.rb',
+  'sugarcube-legacy.rb',
+  'sugarcube-unholy.rb',
+  'sugarcube-anonymous.rb',
+  'sugarcube-awesome.rb',
+  'sugarcube-osx.rb',
+]
 
 dirname = File.dirname(__FILE__)
 Dir.glob(File.join(dirname, '*.rb')).each do |file|
