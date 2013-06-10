1ef2af880cbdca9d3720aae130b78514473cf541
Colin T.A. Gray <colinta@gmail.com>
2013-06-10 08:23:33 -0600

    fixed NSAttributedString specs

--- spec/nsattributedstring_spec.rb
+++ spec/nsattributedstring_spec.rb
@@ -61,10 +61,6 @@ describe 'NSAttributeString' do
       @subject.isEqualToAttributedString('test'.attrd.underline_style(NSUnderlineStyleSingle)).should != true
     end
 
-    it 'should have `superscript`' do
-      @subject.isEqualToAttributedString('test'.attrd.superscript(1)).should != true
-    end
-
     it 'should have `background_color`' do
       @subject.isEqualToAttributedString('test'.attrd.background_color(UIColor.redColor)).should != true
       @subject.isEqualToAttributedString('test'.attrd.bg_color(UIColor.redColor)).should != true
@@ -79,16 +75,8 @@ describe 'NSAttributeString' do
       @subject.isEqualToAttributedString('test'.attrd.ligature(2)).should != true
     end
 
-    it 'should have `baseline_offset`' do
-      @subject.isEqualToAttributedString('test'.attrd.baseline_offset(1)).should != true
-    end
-
     it 'should have `kern`' do
       @subject.isEqualToAttributedString('test'.attrd.kern(1)).should != true
-    end
-
-    it 'should have `link`' do
-      @subject.isEqualToAttributedString('test'.attrd.link('http://github.com'.nsurl)).should != true
     end
 
     it 'should have `stroke_width`' do
@@ -99,56 +87,17 @@ describe 'NSAttributeString' do
       @subject.isEqualToAttributedString('test'.attrd.stroke_color(UIColor.redColor)).should != true
     end
 
-    it 'should have `underline_color`' do
-      @subject.isEqualToAttributedString('test'.attrd.underline_color(UIColor.redColor)).should != true
-    end
-
     it 'should have `strikethrough_style`' do
       @subject.isEqualToAttributedString('test'.attrd.strikethrough_style(NSUnderlineStyleSingle)).should != true
-    end
-
-    it 'should have `strikethrough_color`' do
-      @subject.isEqualToAttributedString('test'.attrd.strikethrough_color(UIColor.redColor)).should != true
     end
 
     it 'should have `shadow`' do
       @subject.isEqualToAttributedString('test'.attrd.shadow(NSShadow.alloc.init.tap{|s|s.shadowOffset = [1,1]})).should != true
     end
 
-    it 'should have `obliqueness`' do
-      @subject.isEqualToAttributedString('test'.attrd.obliqueness(0.5)).should != true
-    end
-
-    it 'should have `expansion`' do
-      @subject.isEqualToAttributedString('test'.attrd.expansion(1)).should != true
-    end
-
-    # don't care about:
-    # it 'should have `cursor`' do
-    #   @subject.isEqualToAttributedString('test'.attrd.cursor()).should != true
-    # end
-
-    it 'should have `tool_tip`' do
-      @subject.isEqualToAttributedString('test'.attrd.tool_tip('test')).should != true
-    end
-
-    it 'should have `marked_clause_segment`' do
-      @subject.isEqualToAttributedString('test'.attrd.marked_clause_segment(1)).should != true
-    end
-
-    # don't care about:
-    # it 'should have `writing_direction`' do
-    #   @subject.isEqualToAttributedString('test'.attrd.writing_direction()).should != true
-    # end
-
     it 'should have `vertical_glyph_form`' do
       @subject.isEqualToAttributedString('test'.attrd.vertical_glyph_form(1)).should != true
     end
-
-    # don't care about:
-    # it 'should have `text_alternatives`' do
-    #   @subject.isEqualToAttributedString('test'.attrd.text_alternatives()).should != true
-    # end
 
   end
 
