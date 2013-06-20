5dfe5605a236ba7fe0e5ee3b38de7346a2c479ba
Colin T.A. Gray <colinta@gmail.com>
2013-06-20 19:51:16 -0400

    back/forward_fiend

--- lib/sugarcube-animations/uiview.rb
+++ lib/sugarcube-animations/uiview.rb
@@ -364,4 +364,25 @@ class UIView
     end
   end
 
+  # Moves the view backwards, similar to what Google has been doing a lot
+  # recently
+  def back_fiend!(options={})
+    scale = options[:scale] || 0.5
+    perspective = options[:perspective] || -0.0005
+    size = options[:size] || -140
+
+    UIView.animation_chain(duration:200.millisecs, options:UIViewAnimationOptionCurveLinear) {
+      self.layer.transform = CATransform3DTranslate(CATransform3DScale(CATransform3D.new(1,0,0,0, 0,1,0,perspective, 0,0,1,0, 0,0,0,1), scale, scale, scale), 0, size, 0)
+    }.and_then(duration:300.millisecs, options:UIViewAnimationOptionCurveLinear) {
+      self.layer.transform = CATransform3DTranslate(CATransform3DScale(CATransform3DIdentity, scale, scale, scale), 0, size, 0)
+    }.start
+  end
+
+  # restores the layer after a call to 'back_fiend!'
+  def forward_fiend!(options={})
+    UIView.animate(options) do
+      self.layer.transform = CATransform3DIdentity
+    end
+  end
+
 end
