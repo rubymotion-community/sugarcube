6daeea95b88a777b38dd4dc9382c59148fb64845
Colin T.A. Gray <colinta@gmail.com>
2013-09-30 10:39:05 -0600

    'pixels' method

--- lib/sugarcube-numbers/numeric.rb
+++ lib/sugarcube-numbers/numeric.rb
@@ -4,6 +4,13 @@ class Numeric
   ##|  MISC
   ##|
 
+  # Uses the screen scale to determine how many pixels (converts "pixels" to
+  # "points")
+  def pixels
+    self.to_f / UIScreen.mainScreen.scale
+  end
+  alias pixel pixels
+
   def percent
     self / 100.0
   end
