c68e7e4010fc6c2243a1071447be9417f802c316
Colin T.A. Gray <colinta@gmail.com>
2013-10-11 12:51:15 -0600

    support alpha in fourth array position

--- lib/sugarcube-color/nsarray.rb
+++ lib/sugarcube-color/nsarray.rb
@@ -5,6 +5,9 @@ class NSArray
     red = self[0] / 255.0
     green = self[1] / 255.0
     blue = self[2] / 255.0
+    if self[3]
+      alpha = self[3]
+    end
     UIColor.colorWithRed(red, green:green, blue:blue, alpha:alpha.to_f)
   end
 
