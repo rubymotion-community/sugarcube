13f53d4f7b0233587a21f4da3ea4ec206f46ed15
Colin T.A. Gray <colinta@gmail.com>
2014-01-31 15:16:52 -0700

    added support for 'key?' suffix on anonymous methods; returns !!obj[key] (always 'true' or 'false')

--- lib/sugarcube-anonymous/anonymous.rb
+++ lib/sugarcube-anonymous/anonymous.rb
@@ -11,22 +11,38 @@ module SugarCube
   #     obj = {
   #       'first_name': 'Katsuyoshi',  # strings
   #       company: 'Ito Soft Design',  # and symbols are supported
+  #       great: true,
   #     }.to_object
   #
   #     obj.first_name
   #     # => 'Katsuyoshi'
   #     obj.company
   #     # => 'Ito Soft Design'
+  #     # adding a '?' will return true/false
+  #     obj.great?
+  #     # => true
+  #     obj.great
+  #     # => true
+  #
   #     obj.bla  # => raises NoMethodError
   #     obj.bla = 'bla'  # => raises NoMethodError
   class Anonymous < Hash
 
     def method_missing(symbol, *args)
       if args.size == 0
+        if symbol.to_s[-1] == '?'
+          symbol = symbol.to_s[0...-1].to_sym
+          return_bool = true
+        else
+          return_bool = false
+        end
         key = symbol
         key = symbol.to_s unless self.include? key
         if self.include?(key)
           self[key] = self[key].to_object
+          if return_bool
+            return !! self[key]
+          end
           return self[key]
         end
       elsif args.size == 1 && /(.*)=$/ =~ symbol.to_s
@@ -45,7 +61,7 @@ module SugarCube
 
     # replace enumerable methods
     alias :anonymous_each :each
-    
+
     def each
       anonymous_each {|k,v| yield k, v.to_object}
     end
--- spec/anonymous_spec.rb
+++ spec/anonymous_spec.rb
@@ -1,7 +1,7 @@
 describe "SugarCube::Anonymous" do
 
   before do
-    @h = SugarCube::Anonymous[{ foo: 'FOO', "bar" => 'BAR' }]
+    @h = SugarCube::Anonymous[{ foo: 'FOO', "bar" => 'BAR', falsey: nil, 'truthy' => 'yes!' }]
   end
 
   describe "NSDictionary#to_object" do
@@ -30,6 +30,14 @@ describe "SugarCube::Anonymous" do
       @h.bar.should == 'BAR'
     end
 
+    it 'should have an #truthy? method, and return true' do
+      @h.truthy?.should == true
+    end
+
+    it 'should have an #falsey? method, and return false' do
+      @h.falsey?.should == false
+    end
+
     it 'should raise NoMethodError on non-existing keys' do
       should.raise(NoMethodError) {
         @h.hoge
@@ -48,6 +56,18 @@ describe "SugarCube::Anonymous" do
     it 'should have a #bar= method, and return "Bar"' do
       @h.bar = 'Bar'
       @h.bar.should == 'Bar'
+    end
+
+    it 'should have a #truthy= method, and return "True" and true' do
+      @h.truthy = 'True'
+      @h.truthy.should == 'True'
+      @h.truthy?.should == true
+    end
+
+    it 'should have a #falsey= method, and return nil and false' do
+      @h.falsey = nil
+      @h.falsey.should == nil
+      @h.falsey?.should == false
     end
 
     it 'should raise NoMethodError on non-existing keys' do
@@ -71,50 +91,50 @@ describe "SugarCube::Anonymous nested case" do
       }
     ]
   end
-  
+
   describe "dictionary" do
-  
+
     it 'should return an instance of SugarCube::Anonymous' do
       @h.dictionary.class.should == SugarCube::Anonymous
     end
-    
+
     it 'should return a hash which contains a string and an array' do
       @h.dictionary.should == { foo: 'FOO', array: [ { is_fine: 'is_fine' } ] }
     end
-    
+
     it 'should not copy again' do
       @h.dictionary.object_id.should == @h.dictionary.object_id
     end
-    
+
     it 'should return is_fine' do
       @h.dictionary.array[0].is_fine.should == 'is_fine'
     end
-    
+
     describe "Enumerable" do
 
       describe "each" do
-      
+
         it 'should return an instance of [SugarCube::Anonymous, SugarCube::AnonymousArray]' do
           a = []
           @h.each{|k,v| a << v.class}
           a[0].should == SugarCube::Anonymous
           a[1].should == SugarCube::AnonymousArray
         end
-        
+
       end
 
       describe "map" do
-      
+
         it 'should return an instance of [SugarCube::Anonymous, SugarCube::AnonymousArray]' do
           a = @h.map{|k,v| v.class}
           a[0].should == SugarCube::Anonymous
           a[1].should == SugarCube::AnonymousArray
         end
-        
+
       end
-      
+
       describe "collect" do
-      
+
         it 'should return an instance of [SugarCube::Anonymous, SugarCube::AnonymousArray]' do
           a = @h.collect{|k,v| v.class}
           a[0].should == SugarCube::Anonymous
@@ -122,98 +142,98 @@ describe "SugarCube::Anonymous nested case" do
         end
 
       end
-      
+
     end
-      
+
   end
 
   describe "array" do
-  
+
     it 'should return an instance of SugarCube::AnonymousArray' do
       @h.array.class.should == SugarCube::AnonymousArray
     end
-    
+
     it 'should not copy again' do
       @h.array.object_id.should == @h.array.object_id
     end
-      
+
     describe "inner object" do
-    
+
       it 'should return an instance of SugarCube::Anonymous' do
         @h.array[0].class.should == SugarCube::Anonymous
       end
-      
+
       it 'should return an instance of SugarCube::Anonymous' do
         @h.array.first.class.should == SugarCube::Anonymous
       end
-      
+
       it 'should return an instance of SugarCube::AnonymousArray' do
         @h.array[1].class.should == SugarCube::AnonymousArray
       end
-      
+
       it 'should return an instance of SugarCube::AnonymousArray' do
         @h.array.last.class.should == SugarCube::AnonymousArray
       end
-    
+
     end
-    
+
     describe "Enumerable" do
 
       describe "each" do
-      
+
         it 'should return an instance of [SugarCube::Anonymous, SugarCube::AnonymousArray]' do
           a = []
           @h.array.each{|e| a << e.class}
           a[0].should == SugarCube::Anonymous
           a[1].should == SugarCube::AnonymousArray
         end
-        
+
       end
-      
+
       describe "each_with_index" do
-      
+
         it 'should return an instance of [0,1]' do
           a = []
           @h.array.each_with_index{|e,i| a << i}
           a.should == [0,1]
         end
-        
+
       end
-      
+
       describe "map" do
-      
+
         it 'should return an instance of [SugarCube::Anonymous, SugarCube::AnonymousArray]' do
           a = @h.array.map{|e| e.class}
           a[0].should == SugarCube::Anonymous
           a[1].should == SugarCube::AnonymousArray
         end
-        
+
       end
-      
+
       describe "collect" do
-      
+
         it 'should return an instance of [SugarCube::Anonymous, SugarCube::AnonymousArray]' do
           a = @h.array.collect{|e| e.class}
           a[0].should == SugarCube::Anonymous
           a[1].should == SugarCube::AnonymousArray
         end
-        
+
       end
-      
+
       describe "find" do
-      
+
         it 'should return then first object' do
           r = @h.array.find{|e| e.is_a? Hash}
           r.should == @h.array[0]
         end
-        
+
         it 'should return the second object' do
           r = @h.array.find{|e| e.is_a? Array}
           r.should == @h.array[1]
         end
-        
+
       end
-      
+
     end
 
   end
