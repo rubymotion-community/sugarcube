fb5356a70882980b34c3a4ad007aee3359d1c4b5
Colin T.A. Gray <colinta@gmail.com>
2014-03-03 10:31:29 -0700

    playing with CAAnimation

--- lib/sugarcube-animations/caanimation.rb
+++ lib/sugarcube-animations/caanimation.rb
@@ -2,7 +2,35 @@ class CAAnimation
 
   class << self
 
+    # If you pass a block, that block will be called at the end of the
+    # animation.
     def basic(target, key_path, options={}, &block)
+      animation = CABasicAnimation.animationWithKeyPath(key_path)
+      animation.duration = options[:duration] if options[:duration]
+      if options[:delegate]
+        animation.delegate = options[:delegate]
+      end
+
+      if options.key?(:from) || options.key?(:to) || options.key?(:by)
+        add_animation = options.fetch(:add, true)
+
+        animation.fromValue = options[:from] if options[:from]
+        animation.toValue = options[:to] if options[:to]
+        animation.byValue = options[:by] if options[:by]
+      else
+        add_animation = options.fetch(:add, false)
+      end
+
+      if add_animation
+        target.addAnimation(animation, forKey:key_path)
+        target.send("#{key_path}=", options[:to])
+      end
+
+      block.call(animation)
+      return animation
+    end
+
+    def keyframe(target, key_path, options={}, &block)
       animation = CABasicAnimation.animationWithKeyPath(key_path)
       animation.duration = options[:duration] if options[:duration]
       animation.delegate = options[:delegate] if options[:delegate]
