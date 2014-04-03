34129df095b3cbc96c006257ac8615dfa2c44157
Colin T.A. Gray <colinta@gmail.com>
2014-04-03 15:35:56 -0600

    more CAAnimation fun - fixed keyframe_animation

--- lib/sugarcube-animations/caanimation.rb
+++ lib/sugarcube-animations/caanimation.rb
@@ -23,34 +23,40 @@ class CAAnimation
 
       if add_animation
         target.addAnimation(animation, forKey:key_path)
-        target.send("#{key_path}=", options[:to])
+        target.setValue(options[:to], forKeyPath: key_path)
       end
 
-      block.call(animation)
       return animation
     end
 
     def keyframe(target, key_path, options={}, &block)
-      animation = CABasicAnimation.animationWithKeyPath(key_path)
+      timing_function = options.fetch(:timing, KCAMediaTimingFunctionLinear)
+      fill_mode = options.fetch(:fill_mode, KCAFillModeForwards)
+      remove_on_completion = options.fetch(:remove, true)
+
+      animation = CAKeyframeAnimation.animationWithKeyPath(key_path)
+      animation.timingFunction = CAMediaTimingFunction.functionWithName(timing_function)
       animation.duration = options[:duration] if options[:duration]
       animation.delegate = options[:delegate] if options[:delegate]
+      animation.duration = duration
+      animation.fillMode = fill_mode
+      animation.removedOnCompletion = remove_on_completion
 
-      if options.key?(:from) || options.key?(:to) || options.key?(:by)
+      if options.key?(:values)
+        values = options.fetch(:values, true)
+        animation.values = values
         add_animation = options.fetch(:add, true)
-
-        animation.fromValue = options[:from] if options[:from]
-        animation.toValue = options[:to] if options[:to]
-        animation.byValue = options[:by] if options[:by]
       else
         add_animation = options.fetch(:add, false)
       end
 
       if add_animation
         target.addAnimation(animation, forKey:key_path)
-        target.send("#{key_path}=", options[:to])
+        if options[:values]
+          target.setValue(options[:values].last, forKeyPath: key_path)
+        end
       end
 
-      block.call(animation) if block
       return animation
     end
 
