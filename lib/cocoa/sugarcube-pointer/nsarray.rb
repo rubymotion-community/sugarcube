2d33d518aac6446c94e8c5eef10d2e1aae3f3dae
Drew Buglione <me@drewb.ug>
2014-04-26 11:22:32 -0600

    Add NSArray#to_pointers

--- lib/cocoa/sugarcube-pointer/nsarray.rb
+++ lib/cocoa/sugarcube-pointer/nsarray.rb
@@ -10,4 +10,15 @@ class NSArray
     ret
   end
 
+  # @param type [Symbol] type A pointer type from the list at {http://www.rubymotion.com/developer-center/guides/runtime/ RubyMotion Pointers Reference#_pointers}
+  # @return [Pointer] An array of pointers of the specified type
+  def to_pointers(type)
+    self.map do |val|
+      ptr = Pointer.new(type)
+      ptr[0] = val
+      ptr
+    end
+  end
+
+
 end
