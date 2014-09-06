4957f08e7fe865947e37b3267982540e63d1558e
Colin T.A. Gray <colinta@gmail.com>
2014-09-06 10:59:02 -0600

    detect ios/osx correctly when compiling an extension

--- lib/sugarcube.rb
+++ lib/sugarcube.rb
@@ -5,13 +5,13 @@ module SugarCube
   module_function
 
   def ios_only!(package)
-    unless App.template == :ios
+    unless App.template.to_s =~ /ios/
       raise "The '\033[0;1msugarcube-#{package}\033[0m' package is only available on iOS."
     end
   end
 
   def osx_only!(package)
-    unless App.template == :osx
+    unless App.template.to_s == /osx/
       raise "The '\033[0;1msugarcube-#{package}\033[0m' package is only available on OS X."
     end
   end
--- lib/version.rb
+++ lib/version.rb
@@ -1,3 +1,3 @@
 module SugarCube
-  Version = '2.0.2'
+  Version = '2.1.0'
 end
