7ca48b1722a82cf6c75409406eabbc0b393b4062
Benjamin Michotte <bmichotte@gmail.com>
2014-09-30 18:41:19 +0200

    More buttons

--- lib/ios/sugarcube-uikit/uibutton.rb
+++ lib/ios/sugarcube-uikit/uibutton.rb
@@ -4,8 +4,27 @@ class UIButton
     titleForState(UIControlStateNormal)
   end
 
+  def title=(value)
+    setTitle(value)
+  end
+
   def setTitle(value)
     setTitle(value, forState: UIControlStateNormal)
   end
 
+  def attr_text
+    attributedTitleForState(UIControlStateNormal)
+  end
+
+  def attr_text=(value)
+    setAttributedTitle(value)
+  end
+
+  def setAttributedTitle(value)
+    setAttributedTitle(value, forState: UIControlStateNormal)
+  end
+
+  def font=(value)
+    self.titleLabel.font = value
+  end
 end
