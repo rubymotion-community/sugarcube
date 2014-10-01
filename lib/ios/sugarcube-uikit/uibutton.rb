53579d40a488c6c5744a39c7be37b56838137479
Colin T.A. Gray <colinta@gmail.com>
2014-10-01 14:11:43 -0600

    some UIButton alias for attributedTitle (attributed_title), and added titleColor/title_color

--- lib/ios/sugarcube-uikit/uibutton.rb
+++ lib/ios/sugarcube-uikit/uibutton.rb
@@ -8,12 +8,25 @@ class UIButton
     setTitle(value, forState: UIControlStateNormal)
   end
 
+  def titleColor
+    titleColorForState(UIControlStateNormal)
+  end
+  alias title_color titleColor
+
+  def setTitleColor(value)
+    value = value.uicolor if value.respond_to?(:uicolor)
+    setTitleColor(value, forState: UIControlStateNormal)
+  end
+  alias title_color= setTitleColor
+
   def attributedTitle
     attributedTitleForState(UIControlStateNormal)
   end
+  alias attributed_title attributedTitle
 
   def setAttributedTitle(value)
     setAttributedTitle(value, forState: UIControlStateNormal)
   end
+  alias attributed_title= setAttributedTitle
 
 end
--- spec/ios/uibutton_spec.rb
+++ spec/ios/uibutton_spec.rb
@@ -19,8 +19,38 @@ describe UIButton do
     @subject.title.should == 'foo'
   end
 
+  it 'should set the titleColor using `titleColor=`' do
+    @subject.titleColor = UIColor.whiteColor
+    @subject.titleColorForState(UIControlStateNormal).should == UIColor.whiteColor
+  end
+
+  it 'should set the titleColor using `title_color=`' do
+    @subject.title_color = UIColor.whiteColor
+    @subject.titleColorForState(UIControlStateNormal).should == UIColor.whiteColor
+  end
+
+  it 'should set the titleColor using `setTitleColor()`' do
+    @subject.setTitleColor(UIColor.whiteColor)
+    @subject.titleColorForState(UIControlStateNormal).should == UIColor.whiteColor
+  end
+
+  it 'should get the titleColor' do
+    @subject.setTitleColor(UIColor.whiteColor, forState: UIControlStateNormal)
+    @subject.titleColor.should == UIColor.whiteColor
+  end
+
+  it 'should get the title_color' do
+    @subject.setTitleColor(UIColor.whiteColor, forState: UIControlStateNormal)
+    @subject.title_color.should == UIColor.whiteColor
+  end
+
   it 'should set the attributedTitle using `attributedTitle=`' do
     @subject.attributedTitle = 'foo'.attrd
+    @subject.attributedTitleForState(UIControlStateNormal).should == 'foo'.attrd
+  end
+
+  it 'should set the attributedTitle using `attributed_title=`' do
+    @subject.attributed_title = 'foo'.attrd
     @subject.attributedTitleForState(UIControlStateNormal).should == 'foo'.attrd
   end
 
@@ -32,6 +62,11 @@ describe UIButton do
   it 'should get the attributedTitle' do
     @subject.setAttributedTitle('foo'.attrd, forState: UIControlStateNormal)
     @subject.attributedTitle.should == 'foo'.attrd
+  end
+
+  it 'should get the attributed_title' do
+    @subject.setAttributedTitle('foo'.attrd, forState: UIControlStateNormal)
+    @subject.attributed_title.should == 'foo'.attrd
   end
 
 end
