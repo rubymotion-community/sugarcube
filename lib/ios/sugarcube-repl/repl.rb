c73d7e0a8201965d0097bdce8b9316cc18f7cddf
Colin T.A. Gray <colinta@gmail.com>
2014-10-07 21:05:49 -0600

    adding android REPL files

--- lib/ios/sugarcube-repl/repl.rb
+++ lib/ios/sugarcube-repl/repl.rb
@@ -1,12 +1,14 @@
 module SugarCube
-  module Repl
-    module_function
+  class << Repl
 
     def window(index=nil)
       if index
         UIApplication.sharedApplication.windows[index]
       else
-        UIApplication.sharedApplication.keyWindow
+        (
+          UIApplication.sharedApplication.keyWindow ||
+          UIApplication.sharedApplication.windows[0]
+        )
       end
     end
 
@@ -16,7 +18,7 @@ module SugarCube
 
     def adjust_init(view)
       if view.is_a?(UIView)
-        @sugarcube_restore = {
+        @restore = {
           frame: SugarCube::Repl.frame,
           shadow: SugarCube::Repl.shadow,
         }
@@ -39,19 +41,19 @@ module SugarCube
             if key == :color and [Symbol, Fixnum, NSString, UIImage, UIColor].any?{|klass| value.is_a? klass}
               value = value.uicolor.CGColor
             end
-            @sugarcube_view.layer.send(msg, value)
-            @sugarcube_view.layer.masksToBounds = false
-            @sugarcube_view.layer.shouldRasterize = true
+            @adjust_item.layer.send(msg, value)
+            @adjust_item.layer.masksToBounds = false
+            @adjust_item.layer.shouldRasterize = true
           end
         }
-        @sugarcube_view
+        @adjust_item
       else
         {
-          opacity: @sugarcube_view.layer.shadowOpacity,
-          radius: @sugarcube_view.layer.shadowRadius,
-          offset: @sugarcube_view.layer.shadowOffset,
-          color: @sugarcube_view.layer.shadowColor,
-          path: @sugarcube_view.layer.shadowPath,
+          opacity: @adjust_item.layer.shadowOpacity,
+          radius: @adjust_item.layer.shadowRadius,
+          offset: @adjust_item.layer.shadowOffset,
+          color: @adjust_item.layer.shadowColor,
+          path: @adjust_item.layer.shadowPath,
         }
       end
     end
@@ -60,15 +62,15 @@ module SugarCube
     def blink(color=nil)
       return unless check_sugarcube_view
 
-      blinking_view = UIView.alloc.initWithFrame([[0,0], @sugarcube_view.frame.size])
+      blinking_view = UIView.alloc.initWithFrame([[0,0], @adjust_item.frame.size])
       color = color.uicolor if color.respond_to?(:uicolor)
       blinking_view.backgroundColor = color
       blinking_view.alpha = 0
-      if @sugarcube_view.window
-        blinking_view.frame = @sugarcube_view.convertRect([[0, 0], @sugarcube_view.frame.size], toView: @sugarcube_view.window)
-        @sugarcube_view.window.addSubview(blinking_view)
+      if @adjust_item.window
+        blinking_view.frame = @adjust_item.convertRect([[0, 0], @adjust_item.frame.size], toView: @adjust_item.window)
+        @adjust_item.window.addSubview(blinking_view)
       else
-        @sugarcube_view.addSubview(blinking_view)
+        @adjust_item.addSubview(blinking_view)
       end
 
       duration = 0.2
@@ -83,19 +85,14 @@ module SugarCube
       end)
     end
 
-    def get_tree_selector(item)
-      if item.is_a? UIView
-        return :subviews
-      elsif item.is_a? UIViewController
-        return -> (ctlr) do
-          ret = Array.new ctlr.childViewControllers
-          if ctlr.presentedViewController && ctlr.presentedViewController.presentingViewController == ctlr
-            ret << ctlr.presentedViewController
-          end
-          ret
+    def register_platform_tree_selectors
+      register_tree_selector(UIView, :subviews)
+      register_tree_selector(UIViewController) do |ctlr|
+        ret = Array.new ctlr.childViewControllers
+        if ctlr.presentedViewController && ctlr.presentedViewController.presentingViewController == ctlr
+          ret << ctlr.presentedViewController
         end
-      else
-        nil
+        ret
       end
     end
 
