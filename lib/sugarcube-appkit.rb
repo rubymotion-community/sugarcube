2070710cd2f10d914553ec31a84c081c891948bf
Colin T.A. Gray <colinta@gmail.com>
2014-10-07 21:06:53 -0600

    finally refactored file including.  uses new RubyMotion method 'pre_setup'

--- lib/sugarcube-appkit.rb
+++ lib/sugarcube-appkit.rb
@@ -6,16 +6,6 @@ end
 require 'sugarcube'
 SugarCube.osx_only!('appkit')
 
-Motion::Project::App.setup do |app|
-  # scans app.files until it finds app/ (the default)
-  # if found, it inserts just before those files, otherwise it will insert to
-  # the end of the list
-  insert_point = app.files.find_index { |file| file =~ /^(?:\.\/)?app\// } || 0
-
-  Dir.glob(File.join(File.dirname(__FILE__), 'osx/sugarcube-appkit/**/*.rb')).reverse.each do |file|
-    app.files.insert(insert_point, file)
-  end
-  Dir.glob(File.join(File.dirname(__FILE__), 'cocoa/sugarcube-cocoakit/**/*.rb')).reverse.each do |file|
-    app.files.insert(insert_point, file)
-  end
+Motion::Project::App.pre_setup do |app|
+  SugarCube.add_app_files(app, 'sugarcube-appkit')
 end
--- lib/sugarcube-repl.rb
+++ lib/sugarcube-repl.rb
@@ -5,22 +5,8 @@ end
 
 require 'sugarcube'
 
-Motion::Project::App.setup do |app|
-  # This package is only available in development
+Motion::Project::App.pre_setup do |app|
   app.development do
-    # scans app.files until it finds app/ (the default)
-    # if found, it inserts just before those files, otherwise it will insert to
-    # the end of the list
-    insert_point = app.files.find_index { |file| file =~ /^(?:\.\/)?app\// } || 0
-
-    Dir.glob(File.join(File.dirname(__FILE__), SugarCube.platform, 'sugarcube-repl/**/*.rb')).reverse.each do |file|
-      app.files.insert(insert_point, file)
-    end
-    Dir.glob(File.join(File.dirname(__FILE__), 'cocoa/sugarcube-repl/**/*.rb')).reverse.each do |file|
-      app.files.insert(insert_point, file)
-    end
-    Dir.glob(File.join(File.dirname(__FILE__), 'all/sugarcube-repl/**/*.rb')).reverse.each do |file|
-      app.files.insert(insert_point, file)
-    end
+    SugarCube.add_app_files(app, 'sugarcube-repl')
   end
 end
