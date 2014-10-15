fb112ed4a2c47607d52a2325d096645496cb1abc
Colin T.A. Gray <colinta@gmail.com>
2014-10-15 15:59:14 -0600

    don't use weird 'loadView' syntax, just use simple viewDidLoad

--- app-ios/app_delegate.rb
+++ app-ios/app_delegate.rb
@@ -16,13 +16,11 @@ end
 
 class MyController < UIViewController
 
-  def loadView
-    super.tap do
-      @label = ('Hi!'.underline + '2'.attrd.superscript).uilabel
-      @label.center = self.view.center
-      @label.textColor = :white.uicolor
-      self.view << @label
-    end
+  def viewDidLoad
+    @label = ('Hi!'.underline + '2'.attrd.superscript).uilabel
+    @label.center = self.view.center
+    @label.textColor = :white.uicolor
+    self.view << @label
   end
 
 end
