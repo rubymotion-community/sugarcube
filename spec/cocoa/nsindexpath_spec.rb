ba7fdf509ca569176dc453cd9d93d9ad4b2a53eb
Colin T.A. Gray <colinta@gmail.com>
2014-12-05 11:20:18 -0700

    more NSIndexPath tests, for funsies

--- spec/cocoa/nsindexpath_spec.rb
+++ spec/cocoa/nsindexpath_spec.rb
@@ -11,4 +11,16 @@ describe NSIndexPath do
     b.should == 3
   end
 
+  it 'should support indexPathForRow(inSection:)' do
+    path = NSIndexPath.indexPathForRow(1, inSection: 3).to_a
+    path[0].should == 3
+    path[1].should == 1
+  end
+
+  it 'should support indexPathForItem(inSection:)' do
+    path = NSIndexPath.indexPathForItem(1, inSection: 3).to_a
+    path[0].should == 3
+    path[1].should == 1
+  end
+
 end
