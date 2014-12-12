13f9b7e4f79a15263f050d491bf2d81686d313ea
Colin T.A. Gray <colinta@gmail.com>
2014-12-12 11:27:28 -0700

    removing these old package names in 3.0

--- lib/sugarcube-uicolor.rb
+++
@@ -1,1 +0,0 @@
-raise "I changed the 'sugarcube-\033[0;1muicolor\033[0m' package to 'sugarcube-\033[0;1mcolor\033[0m'."
--- lib/sugarcube-uiimage.rb
+++
@@ -1,1 +0,0 @@
-raise "I changed the 'sugarcube-\033[0;1muiimage\033[0m' package to 'sugarcube-\033[0;1mimage\033[0m'."
