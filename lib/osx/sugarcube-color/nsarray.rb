b61c6d987dbc50fbbe2d5b245f0f3f88e5aff74e
Colin T.A. Gray <colinta@gmail.com>
2015-03-12 14:38:50 -0600

    fixed OSX '[].nscolor' method, and related spec updates

--- lib/osx/sugarcube-color/nsarray.rb
+++ lib/osx/sugarcube-color/nsarray.rb
@@ -1,7 +1,7 @@
 class NSArray
 
   # [160, 210, 242].uicolor => 0xA0D2F2.uicolor
-  def uicolor(alpha=1.0)
+  def nscolor(alpha=1.0)
     red = self[0] / 255.0
     green = self[1] / 255.0
     blue = self[2] / 255.0
